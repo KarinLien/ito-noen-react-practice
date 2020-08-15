76b7e1e974cde0bf3f6acd1b85f0f592dbfe68d4
karin <karin@karinde-MacBook-Pro.local>
2020-08-15 14:16:16 +0800

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Route, BrowserRouter } from "react-router-dom";
+import { Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Purchase from "./pages/Purchase";
 import About from "./pages/About";
@@ -15,19 +15,25 @@ import Plantation from "./pages/Plantation";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/purchase" component={Purchase} />
-      <Route path="/purchase/fruit" component={PurchaseFruit} />
-      <Route path="/purchase/juice" component={PurchaseJuice} />
-      <Route path="/about" component={About} />
-      <Route exact path="/business" component={Business} />
-      <Route path="/bussiness/plantation" component={Plantation} />
-      <Route path="/philosophy" component={Philosophy} />
-      <Route path="/outline" component={Outline} />
-      <Route path="/recruit" component={Recruit} />
-      <Route path="/products" component={Product} />
-    </BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <Route
+        render={({ location }) => (
+          <Switch location={location}>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/purchase" component={Purchase} />
+            <Route path="/purchase/fruit" component={PurchaseFruit} />
+            <Route path="/purchase/juice" component={PurchaseJuice} />
+            <Route path="/about" component={About} />
+            <Route exact path="/business" component={Business} />
+            <Route path="/bussiness/plantation" component={Plantation} />
+            <Route path="/philosophy" component={Philosophy} />
+            <Route path="/outline" component={Outline} />
+            <Route path="/recruit" component={Recruit} />
+            <Route path="/products" component={Product} />ƒ
+          </Switch>
+        )}
+      />
+    </HashRouter>
   );
 }
 
